ad2f9294a7fa533b444ed50db5aa1c7bedd971ac
yangguang_yk <371807984@qq.com>
2019-04-18 16:22:20 +0800

    添加创建离开房间

--- javascript/client.js
+++ javascript/client.js
@@ -10,20 +10,34 @@
     userName: socket.userName
   });
   //登陆成功返回，加入房间
-  socket.on("login success", function(obj) {
-    console.log("login success: ", obj);
+  socket.on("login_success", function(obj) {
+    console.log("登陆成功: ", obj);
     socket.userId = obj.userId;
-    socket.emit("join room", {
-      userId: socket.userId,
-      userName: socket.userName
-    });
+    // socket.emit("create_room",{roomName:"新房间"});
+    socket.emit("join_room", { roomId: 1000 });
   });
   //新用户登陆
-  socket.on("new user login", obj => {
-    console.log("new user login: ", obj);
+  socket.on("other_user_login", obj => {
+    console.log("其他用户登陆成功: ", obj);
+  });
+  //创建房间
+  socket.on("create_room_success", obj => {
+    console.log("创建房间成功: ", obj);
+  });
+  //加入房间
+  socket.on("join_room_success", obj => {
+    console.log("加入房间成功: ", obj);
+  });
+  //离开房间
+  socket.on("leave_room_success", obj => {
+    console.log("离开房间成功: ", obj);
+  });
+  //用户断开连接
+  socket.on("other_disconnect", obj => {
+    console.log("other_disconnect: ", obj);
   });
   //加入房间成功
-  socket.on("a new user has joined the room", obj => {
+  socket.on("other_joined_room", obj => {
     console.log("a new user has joined the room: ", obj);
   });
 
